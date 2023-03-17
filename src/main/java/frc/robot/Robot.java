37bfdf6c8ca54786ababa44b29a23d9b4065538d
Nicolas Fisher <nicofisher2007@gmail.com>
2023-03-17 13:09:18 -0700

    Add CameraServer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
 
 import com.pathplanner.lib.server.PathPlannerServer;
 
@@ -33,6 +34,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    CameraServer.startAutomaticCapture();
     PathPlannerServer.startServer(5811); //TODO: get rid of thise before comp
   }
 
